0df605334a5f8e2b49698887a7a25ae1c1ad6072
cstefann <thecomputerguy941@gmail.com>
2021-03-17 10:31:53 +0200

    Alpha version of YouTube scraper. (loads a part of the comments)

---
+++ scripts/yt_scraper.py
@@ -0,0 +1,30 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# using chrome driver for browser emulation
+
+driver_exe = 'chromedriver'
+
+# selenium by default opens a web view -- for hiding that we'll use headless parameter
+
+url = input("Enter YT video URL:\n")
+
+options = Options()
+options.add_argument("--headless") 
+driver = webdriver.Chrome(driver_exe, options=options)
+wait = WebDriverWait(driver,10)
+driver.get(url)
+
+for item in range(5): # increase => more content
+    wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
+    time.sleep(3)
+
+for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#comment #content-text"))):
+    print(comment.text)
+
