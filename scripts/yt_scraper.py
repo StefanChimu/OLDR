33adcd950407e874e0f08199c750965fb2aee497
cstefann <thecomputerguy941@gmail.com>
2021-03-30 09:46:37 +0300

    Added emoji removal + bigger output.

--- scripts/yt_scraper.py
+++ scripts/yt_scraper.py
@@ -1,3 +1,4 @@
+import re
 import time
 from selenium import webdriver
 from selenium.webdriver import Chrome
@@ -6,6 +7,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+def remove_emoji(str):
+    return str.encode('ascii', 'ignore').decode('ascii')
 
 # using chrome driver for browser emulation
 
@@ -21,10 +25,9 @@ driver = webdriver.Chrome(driver_exe, options=options)
 wait = WebDriverWait(driver,10)
 driver.get(url)
 
-for item in range(5): # increase => more content
+for item in range(50): # increase => more content
     wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
     time.sleep(3)
 
 for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#comment #content-text"))):
-    print(comment.text)
-
+    print(remove_emoji(comment.text))
