f3d0319b83379612d2eaee50ba466b77f0a420fc
cstefan <33597024+StefanChimu@users.noreply.github.com>
2021-04-20 18:52:58 +0300

    Added csv exports + func to delete other symbols

--- scripts/stopwords.py
+++ scripts/stopwords.py
@@ -1,12 +1,18 @@
 import io
+import re
 import csv
 import nltk
 import pickle
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-nltk.download('stopwords')
-nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('punkt')
+
+from nltk.stem.snowball import SnowballStemmer
+
+stm = SnowballStemmer("romanian") # used for stemming words
 
 def elim_diacritics(text):
     text = text.replace('â', 'a')
@@ -14,29 +20,20 @@ def elim_diacritics(text):
     text = text.replace('Ă', 'A')
     text = text.replace('Â', 'A')
     text = text.replace('î', 'i')
-    text = text.replace('Î', 'I')
+    text = text.replace('î', 'I')
     text = text.replace('ț', 't')
     text = text.replace('Ț', 'T')
     text = text.replace('ș', 's')
     text = text.replace('Ș', 'S')
     return text
 
-def elim_punct(text):
-    text = text.replace('.', '')
-    text = text.replace(',', '')
-    text = text.replace('?', '')
-    text = text.replace('!', '')
-    text = text.replace(':', '')
-    text = text.replace(';', '')
-    text = text.replace('"', '')
-    text = text.replace("'", "")
-    text = text.replace('(', '')
-    text = text.replace(')', '')
-    text = text.replace('[', '')
-    text = text.replace(']', '')
-    text = text.replace('{', '')
-    text = text.replace('}', '')
-    return text
+def elim_noise(text):
+    noises = ['.', ',', '(', ')', '[', ']', '{', '}', '@', '!', '#', '$', '%', '^', '&', '*', '-', '+', ':', ';', '/', '`', '"', "'"]
+
+    for noise in noises:
+        text = text.replace(noise, '')
+
+    return re.sub(r'[^a-zA-Z]', ' ', text)
 
 def cleanup(text):
     setter = 0
@@ -51,13 +48,10 @@ def cleanup(text):
                 new_string = new_string + text[i]
     return new_string
 
-
 def mainfunc(text):
     checker = 0
 
-    f = io.open('output.txt', "w", encoding="utf-8")
-
-    filtered_text = elim_punct(elim_diacritics(cleanup(str(text))))
+    filtered_text = elim_noise(elim_diacritics(cleanup(str(text))))
 
     stop_words = set(stopwords.words('romanian'))
 
@@ -71,14 +65,18 @@ def mainfunc(text):
         if w not in stop_words:
             filtered_sentence.append(w)
 
+    clean_output = []
+
     for w in word_tokens:
         if w not in stop_words:
             if w != "„" and w != "”" and w != "«" and w != "»" and w != "[" and w != "]" and w != "…" and w != "km²":
-                f.writelines(w)
-                f.write("\n")
+                if len(stm.stem(w)) > 1:
+                    clean_output.append(stm.stem(w))
                 if checker == 0:
                     checker = 1
-    f.close()
+
+    output = pd.DataFrame(clean_output, columns=['Words'])
+    output.to_csv('train_data.csv', index=False)
 
     if checker == 1:
         print("\nDone with succes! Check the output file!\n")
